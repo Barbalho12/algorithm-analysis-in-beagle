9bf206b7af7b23484daca1c445cc95fe5ee9c820
Breno Viana <brenomauriciofv@gmail>
2016-03-03 16:37:11 -0300

    Implementando a verficação do tempo e do clock.

--- AnalysisTime/main.cpp
+++ AnalysisTime/main.cpp
@@ -1,6 +1,6 @@
 /*
  * Universidade Federal do Rio Grande do Norte
- * Instituto Metropole Digital
+ * Instituto Metrópole Digital
  *
  * copyright: Ivanovitch Silva
  * e-mail: ivan@imd.ufrn.br
@@ -11,52 +11,89 @@
 
 #include <cstdlib>
 #include <cstdio>
-#include <time.h>
+#include <ctime>
 #include "bubblesort.h"
 #include "quicksort.h"
 #include "mergesort.h"
 
 using namespace std;
 
-//Vetores usados pelos metodos de ordenacao
+// Vetores usados pelos metodos de ordenacao
 int *vetorQuickSort;
 int *vetorBubbleSort;
 int *vetorMergeSort;
 int tamanho;
 
-
-//Observe que os numeros sao gerados aleatorios baseados
-//em uma semente. Se for passado a mesma semente, os
-//numeros aleatarios serao os mesmos
-void criarVetor(int tamanhoVetor, int semente){
-  srand (semente);
+/**
+ * Observe que os números sao gerados aleatórios baseados
+ * em uma semente. Se for passado a mesma semente, os
+ * números aleatórios serão os mesmos.
+ */
+void criarVetor(int tamanhoVetor) {
+  srand(time(NULL));
   vetorQuickSort = new int[tamanhoVetor];
   vetorBubbleSort = new int[tamanhoVetor];
   vetorMergeSort = new int[tamanhoVetor];
-  for (int i=0;i<tamanhoVetor;i++){
-    vetorQuickSort[i] =  rand()%100000;
-    vetorBubbleSort[i] = rand()%100000;
-    vetorMergeSort[i] = rand()%100000;
+  // Preenche os vetores
+  for (int i = 0; i < tamanhoVetor; i++) {
+    vetorQuickSort[i] =  rand() % 100000;
+    vetorBubbleSort[i] = rand() % 100000;
+    vetorMergeSort[i] = rand() % 100000;
   }
 }
 
-
+/**
+ * Método principal.
+ */
 int main(int argc, char *argv[]){
-  //Tamanho do vetor
-  int n = 10;
-  //Criar vetor com elementos aleatorios[0,100000]
-  criarVetor(n,23);
+  // Tamanho do vetor
+  int size = 10;
+  // Criar vetor com elementos aleatórios[0, 100000]
+  criarVetor(size);
 
-  //Ordenar utilizando bubleSort
+  std::cout << vetorQuickSort << std::endl;
+  std::cout << vetorBubbleSort << std::endl;
+  std::cout << vetorMergeSort << std::endl;
+
+  // Ordenar utilizando BubleSort
   BubbleSort bs = BubbleSort();
-  bs.start(vetorBubbleSort,n);
+  time_t time_buble;
+  clock_t clock_buble = clock();
+  time(&time_buble);
+  bs.start(vetorBubbleSort,size);
+  time_t time_buble_end;
+  clock_buble = clock() - clock_buble;
+  time(&time_buble_end);
+  printf ("Start time: %s", ctime (&time_buble));
+  printf ("End time: %s", ctime (&time_buble_end));
+  printf ("It took me %d clicks (%f seconds).\n",
+  clock_buble, ((float)clock_buble)/CLOCKS_PER_SEC);
 
-  //Ordenar utilizando quickSort
+  // Ordenar utilizando QuickSort
   QuickSort qs = QuickSort();
-  qs.start(vetorQuickSort, n);
+  time_t time_quick;
+  clock_t clock_quick = clock();
+  time(&time_quick);
+  qs.start(vetorQuickSort, size);
+  time_t time_quick_end;
+  clock_quick = clock() - clock_quick;
+  time(&time_quick_end);
+  printf ("Start time: %s", ctime (&time_quick));
+  printf ("End time: %s", ctime (&time_quick_end));
+  printf ("It took me %d clicks (%f seconds).\n",
+  clock_quick, ((float)clock_quick)/CLOCKS_PER_SEC);
 
-  //Ordenar utilizando mergeSort
+  // Ordenar utilizando MergeSort
   MergeSort ms = MergeSort();
-  ms.start(vetorMergeSort, n);
+  time_t time_merge;
+  clock_t clock_merge = clock();
+  time(&time_merge) = clock();
+  ms.start(vetorMergeSort, size);
+  clock_merge = clock() - clock_merge;
+  time(&time_merge_end);
+  printf ("Start time: %s", ctime (&time_merge));
+  printf ("End time: %s", ctime (&time_merge_end));
+  printf ("It took me %d clicks (%f seconds).\n",
+  clock_merge, ((float)clock_merge)/CLOCKS_PER_SEC);
   return 0;
 }
